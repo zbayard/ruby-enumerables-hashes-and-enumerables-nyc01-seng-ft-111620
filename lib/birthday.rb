6c625a7c46c1e8d3d577ce84cfa4ac67f0a15f9b
Zach Bayard <zbayard@gmail.com>
2020-10-29 15:22:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do | key , value |
-    puts "Happy Birthday #{key}!: You are now #{value} years old!"
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
   end
   # add your code snippet here!
 end
