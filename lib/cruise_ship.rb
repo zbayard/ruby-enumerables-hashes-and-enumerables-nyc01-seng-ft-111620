7a472ff3873d4a33d68a46b90fbd964a3c8f0cac
Zach Bayard <zbayard@gmail.com>
2020-10-29 15:25:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,16 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
+  passengers.each do | key , value |
+    if key == "suite_a"
+      puts value
+    end
+
   # add the code snippet here!
 end
