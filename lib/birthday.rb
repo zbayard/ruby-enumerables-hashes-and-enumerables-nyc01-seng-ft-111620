05d97b37e69839c9436cfe0d8939ddba25774107
Zach Bayard <zbayard@gmail.com>
2020-10-29 15:19:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,5 +7,6 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each_with_index do | winner , age |
     puts "Happy Birthday #{winner}!  You are now #{age} years old!"
+  end
   # add your code snippet here!
 end
